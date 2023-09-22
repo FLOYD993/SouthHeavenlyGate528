391a4c6579c2fcac4d225207f82160b6f0112fec
XiaoliZhouIJ <xiaoli.zhou.ch@outlook.com>
2023-09-22 22:51:34 +0800

    更新代码注释

--- SouthHeavenlyGate528/Assets/Scripts/EnemyFSM/FSM.cs
+++ SouthHeavenlyGate528/Assets/Scripts/EnemyFSM/FSM.cs
@@ -4,52 +4,56 @@ using System.Collections.Generic;
 using System.Data.Common;
 using UnityEngine;
 
+/// <summary>
+/// 敌人所有状态
+/// </summary>
 public enum StateType
 {
-    Idle,
-    Patrol,
-    Chase,
-    React,
-    Attack,
-    Hit,
-    Dead
+    Idle,           // 站立（闲置）
+    Patrol,         // 巡航
+    Chase,          // 追踪
+    React,          // 发现玩家，反应
+    Attack,         // 攻击
+    Hit,            // 受到攻击
+    Dead            // 死亡
 }
 
-[Serializable]
+[Serializable]      // 使该类各参数可以在细节面板中显示
 public class Parameter
 {
-    public int health;
-    public float moveSpeed;
-    public float chaseSpeed;
-    public float idleTime;
+    public int health;                          // 血量
+    public float moveSpeed;                     // 巡航速度
+    public float chaseSpeed;                    // 追踪速度
+    public float idleTime;                      // 站立时间
 
     public Transform[] patrolPoints;            // 巡逻范围
     public Transform[] chasePoints;             // 追击范围
     
-    public Transform target;
-    public LayerMask targetLayer;
-    public Transform attackPoint;
-    public float attackArea;
+    public Transform target;                    // 追踪目标
+    public LayerMask targetLayer;               // 追踪目标层级，通常为Player
+
+    public Transform attackPoint;               // 攻击点（攻击范围圆心）
+    public float attackArea;                    // 攻击范围
     
-    public Animator animator;
+    public Animator animator;                   // 动画控制器
 
-    public bool getHit;
-
-   
+    public bool getHit;                         // 是否收到伤害，用于调试
 }
 
 public class FSM : MonoBehaviour
 {
-    public Parameter parameter;
+    public Parameter parameter;         // 数值集
 
-    private IState currentState;
-
-    private Dictionary<StateType, IState> states = new Dictionary<StateType, IState>();
-
+    private IState currentState;        // 状态
     public IState CurrentState { get => currentState; set => currentState = value; }
+
+    private Dictionary<StateType, IState> states = new Dictionary<StateType, IState>();         // 状态集字典，<key: 状态, value: 状态控制>
+
+
 
     private void Start()
     {
+        // 注册状态
         states.Add(StateType.Idle, new IdleState(this));
         states.Add(StateType.Patrol, new PatrolState(this));
         states.Add(StateType.Chase, new ChaseState(this));
@@ -58,17 +62,19 @@ public class FSM : MonoBehaviour
         states.Add(StateType.Hit, new HitState(this));
         states.Add(StateType.Dead, new DeadState(this));
 
-
+        // 初始状态为站立
         TransitionState(StateType.Idle);
 
+        // 在外部赋值了，这里不再查找
         // parameter.animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        // 当前状态存在
         if (CurrentState != null)
         {
-            CurrentState.OnUpdate();
+            CurrentState.OnUpdate();            // 执行当前状态的OnUpdate方法
         }
 
         if(Input.GetKeyDown(KeyCode.Return))
--- SouthHeavenlyGate528/Assets/Scripts/PlayerController.cs
+++ SouthHeavenlyGate528/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,22 @@ using UnityEngine.TextCore.Text;
 
 public class PlayerController : MonoBehaviour
 {
+    /// <summary>
+    /// 玩家角色
+    /// 后续可能有武器等等
+    /// </summary>
     private Transform charactor;
 
+    /// <summary>
+    /// 玩家动画控制器
+    /// </summary>
     private Animator playerAnimator;
 
     private void Start()
     {
+        // 初始化查找角色
         charactor = transform.Find("Charactor");
+        // 初始化查找动画控制那个hi其
         playerAnimator = charactor.Find("CharctorMesh").GetComponent<Animator>();
 
         // Debug.Log(playerAnimator);
